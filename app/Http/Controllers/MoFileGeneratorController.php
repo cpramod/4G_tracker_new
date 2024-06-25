38c582ec7a4ea9f5121d4bbdfd583d0c718620b2
sigmoidanalytics <benedick.agdipa@gmail.com>
2024-06-25 23:18:15 +1000

    Update MoFileGeneratorController.php

--- app/Http/Controllers/MoFileGeneratorController.php
+++ app/Http/Controllers/MoFileGeneratorController.php
@@ -11,6 +11,7 @@ use App\Services\FreqTuningService;
 class MoFileGeneratorController extends Controller
 {
     protected $freqTuningService;
+
     public function __construct(FreqTuningService $freqTuningService)
     {
         $this->freqTuningService = $freqTuningService;
@@ -26,51 +27,18 @@ class MoFileGeneratorController extends Controller
         $validator = Validator::make($request->all(), [
             'import_file' => 'required|mimes:xlsx',
         ]);
+
         if ($validator->fails()) {
-            return response()->json(['error' => array('message' => $validator->errors()->first())], 500);
+            return response()->json(['error' => ['message' => $validator->errors()->first()]], 500);
         }
+
         $file = $request->file('import_file');
         $fileName = now()->timestamp . "_{$file->getClientOriginalName()}";
         $filePath = $file->storeAs('generator', $fileName);
         $absolutePath = storage_path('app/' . $filePath);
-        $response = $this->freqTuningService->generateScript($absolutePath);
-        // $response = $this->runPythonScript($absolutePath);
-        return $response;
-    }
-    // public function runPythonScript($filePath)
-    // {
-    //     $scriptPath = storage_path('python/enm_script_generator.py ' . $filePath);
-    //     $venvActivatePath = storage_path('python/venv/bin/activate'); // For macOS/Linux
-    //     $command = "source $venvActivatePath && python $scriptPath";
-    //     $output = shell_exec($command);
-    //     $filePaths = json_decode($output, true);
-    //     $filePathsWithUrl = [];
-    //     if (is_array($filePaths)) {
-    //         foreach ($filePaths['filePaths'] as $value) {
-    //             $filePathsWithUrl[] = url('/') . '/' . $value;
-    //         }
-    //     }
-    //     return response()->json([
-    //         'output' => $filePaths ? array('filePaths' => $filePathsWithUrl) : ['error' => 'Something went wrong'],
-    //     ]);
-    // }
-    public function runPythonScript($filePath)
-    {
-        $scriptPath = storage_path('python/enm_script_generator.py ' . $filePath);
-        $venvActivatePath = storage_path('python/venv/bin/activate'); // For macOS/Linux
 
-        $pythonPath = storage_path('python/venv/bin/python');
-        $command = "$pythonPath $scriptPath";
-        $output = shell_exec($command);
-        $filePaths = json_decode($output, true);
-        $filePathsWithUrl = [];
-        if (is_array($filePaths)) {
-            foreach ($filePaths['filePaths'] as $value) {
-                $filePathsWithUrl[] = url('/') . '/' . $value;
-            }
-        }
-        return response()->json([
-            'output' => $filePaths ? array('filePaths' => $filePathsWithUrl) : ['error' => 'Something went wrong'],
-        ]);
+        $response = $this->freqTuningService->generateScript($absolutePath);
+
+        return $response;
     }
 }
